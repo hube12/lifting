55e48a70a416914792742084e743d6d80b5cc6f5
Neil <h66r10@gmail.com>
2020-11-23 01:45:31 +0100

    Example

--- src/main/java/lifting/Main.java
+++ src/main/java/lifting/Main.java
@@ -20,9 +20,11 @@ public class Main {
 
         List<Data> dataList = new ArrayList<>();
         ChunkRand rand = new ChunkRand();
-        dataList.add(new Data(OUTPOST, 19, 2));
-        dataList.add(new Data(VILLAGE, 15, 9));
-        dataList.add(new Data(VILLAGE, 9, 1));
+        dataList.add(new Data(VILLAGE, -32,11));
+        dataList.add(new Data(VILLAGE, -22, 128));
+        dataList.add(new Data(VILLAGE, -29, 1319));
+        dataList.add(new Data(VILLAGE, -350, -26));
+        dataList.add(new Data(VILLAGE, -333, -2611));
         for (Data data : dataList) {
             System.out.println(data);
         }
